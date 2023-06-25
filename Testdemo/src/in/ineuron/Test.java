f5aa5a7cee6b0660c8c62feb3e485a5dc8c17142
sannymca <sannygoswami366@gmail.com>
2023-06-25 20:34:58 +0530

    test

---
+++ Testdemo/src/in/ineuron/Test.java
@@ -0,0 +1,16 @@
+package in.ineuron;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int arr[]=new int[] {10,20,30,40};
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ Testdemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author niet
+ *
+ */
+module Testdemo {
+}
